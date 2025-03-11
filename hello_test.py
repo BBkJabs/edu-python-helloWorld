cf421bdb311cb2c3a79916fc38256d18ffb72d3d
M. Jabs <m.jabs@bbk.schule>
2025-03-11 13:16:50 +0100

    Ausgabe bei Tests übersichtlicher

--- hello_test.py
+++ hello_test.py
@@ -1,17 +1,15 @@
 import hello;
 import io;
 
-def test_hello():
-    #monkeypatch.setattr('sys.stdin', io.StringIO('3 2 1 nn bb aa'))
+def test_hello(capsys):
     hello.main() 
     captured = capsys.readouterr()
 
     expected_output = "Hello\nWorld!"
+    expected_output = expected_output.replace('\r\n', '\n').strip()
     actual_output = captured.out
-
-    # Schleife über alle Zeilen in actual_output; dabie werden in jeder Zeile führende und nachfolgende Leerzeichen entfernt
     actual_output = '\n'.join(line.strip() for line in actual_output.splitlines())
-    
-    # Normalize line endings and remove trailing spaces at line endings
-    assert actual_output.replace('\r\n', '\n').strip() == expected_output.replace('\r\n', '\n').strip()
+    actual_output = actual_output.replace('\r\n', '\n').strip()
+
+    assert actual_output == expected_output
  

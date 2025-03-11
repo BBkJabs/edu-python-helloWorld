20eb5595313239e29c2dbdfd941d39a7a4057560
M. Jabs <m.jabs@bbk.schule>
2025-03-11 13:24:01 +0100

    Korrektur am Test

--- hello_test.py
+++ hello_test.py
@@ -5,7 +5,7 @@ def test_hello(capsys):
     hello.main() 
     captured = capsys.readouterr()
 
-    expected_output = "Hello\nWorld!"
+    expected_output = "Hello World!"
     expected_output = expected_output.replace('\r\n', '\n').strip()
     actual_output = captured.out
     actual_output = '\n'.join(line.strip() for line in actual_output.splitlines())
